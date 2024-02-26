2a874e40422f196bccd2f3abd3fcf83902b7c539
MakiseEmi <quchngp@gmail.com>
2024-02-27 00:51:23 +0700

    custom mapping to get full name of employee

--- src/main/java/myproject/empoyee/mapper/EmployeeMapper.java
+++ src/main/java/myproject/empoyee/mapper/EmployeeMapper.java
@@ -3,14 +3,22 @@ package myproject.empoyee.mapper;
 import myproject.empoyee.dto.EmployeeDTO;
 import myproject.empoyee.entity.Employee;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.Named;
 
 import java.util.List;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.CDI)
 public interface EmployeeMapper {
+    @Mapping(target = "fullName", source = ".", qualifiedByName = "getFullName")
     EmployeeDTO toEmployeeDTO(Employee employee);
 
     Employee toEmployee(EmployeeDTO employeeDTO);
-    List<EmployeeDTO> toEmployeesDTO(List<Employee> employees);
+    List<EmployeeDTO> toEmployeeDTOs(List<Employee> employees);
+
+    @Named("getFullName")
+    default String getFullName(Employee employee) {
+        return String.format("%s %s %s", employee.getLastName(), employee.getMiddleName(), employee.getFirstName());
+    }
 }
