5ea9a3858fcd1406fd4bdc076ceadc18398363dc
MakiseEmi <quchngp@gmail.com>
2024-02-27 00:04:13 +0700

    fix some issues in data insertion sql

--- src/main/resources/db/migration/V1.2__insert_emp_dept_data.sql
+++ src/main/resources/db/migration/V1.2__insert_emp_dept_data.sql
@@ -5,7 +5,7 @@ VALUES
 
 INSERT INTO employees(date_of_birth, first_name, middle_name, last_name, gender, salary, deptid)
 VALUES
-    ('2001-06-21', 'Hung', 'Quoc', 'Pham', 'male', '10', '1'),
-    ('2001-05-21', 'Hoang', 'Van', 'Nguyen', 'male', '10', '2'),
-    ('2001-08-15', 'Linh', 'Thi', 'Tran', 'female', '10', '1'),
-    ('2001-03-04', 'Cuong', 'Tuan', 'Le', 'male', '10', '2');
+    ('2001-06-21', 'Hung', 'Quoc', 'Pham', 'MALE', '10', '1'),
+    ('2001-05-21', 'Hoang', 'Van', 'Nguyen', 'MALE', '10', '2'),
+    ('2001-08-15', 'Linh', 'Thi', 'Tran', 'FEMALE', '10', '1'),
+    ('2001-03-04', 'Cuong', 'Tuan', 'Le', 'MALE', '10', '2');
