a7b2354deb246395879c0f085a7425de0c6d6ac4
MakiseEmi <quchngp@gmail.com>
2024-03-13 01:09:22 +0700

    use swagger to write api doc in the project

--- src/main/java/myproject/empoyee/rest/EmployeeController.java
+++ src/main/java/myproject/empoyee/rest/EmployeeController.java
@@ -1,5 +1,9 @@
 package myproject.empoyee.rest;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import myproject.base.exception.NotFoundException;
 import myproject.empoyee.dto.EmployeeRequestDTO;
 import myproject.empoyee.service.EmployeeService;
@@ -21,23 +25,42 @@ import javax.ws.rs.core.Response;
 
 @Path("employees")
 @Produces(MediaType.APPLICATION_JSON)
+@Api(tags = "Employees")
 public class EmployeeController {
     @Inject
     private EmployeeService employeeService;
 
     @GET
+    @ApiOperation(value = "Get a list of employees")
+    @ApiResponses({
+            @ApiResponse(message = "Return successfully", code = 200),
+            @ApiResponse(message = "Something wrong in the server", code = 500)
+    })
     public Response getEmployees(@QueryParam("isDesc") @DefaultValue("true") boolean isDesc) {
         return Response.ok().entity(employeeService.getAllEmployees(isDesc)).build();
     }
 
     @GET
     @Path("/{employeeId}")
+    @ApiOperation(value = "Get employee by id")
+    @ApiResponses({
+            @ApiResponse(message = "Return a employee successfully", code = 200),
+            @ApiResponse(message = "Cannot found employee by the given id", code = 404),
+            @ApiResponse(message = "Something wrong in the server", code = 500)
+    })
     public Response findEmployeeById(@PathParam("employeeId") Long employeeId) throws NotFoundException {
         return Response.ok().entity(employeeService.findEmployeeById(employeeId)).build();
     }
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Add a new employee")
+    @ApiResponses({
+            @ApiResponse(message = "Return the successfully added employee", code = 200),
+            @ApiResponse(message = "Bad request", code = 400),
+            @ApiResponse(message = "The employee already exists", code = 409),
+            @ApiResponse(message = "Something wrong in the server", code = 500)
+    })
     public Response addEmployee(@Valid EmployeeRequestDTO request) throws NotFoundException, ValidationException {
         return Response.ok().entity(employeeService.addEmployee(request)).build();
     }
@@ -45,6 +68,12 @@ public class EmployeeController {
     @PUT
     @Path("/{employeeId}")
     @Consumes(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Update an employee")
+    @ApiResponses({
+            @ApiResponse(message = "Return the successfully updated employee", code = 200),
+            @ApiResponse(message = "Bad request", code = 400),
+            @ApiResponse(message = "Something wrong in the server", code = 500)
+    })
     public Response updateEmployee(
             @Valid EmployeeRequestDTO request,
             @PathParam("employeeId") Long employeeId) throws NotFoundException, ValidationException {
@@ -53,6 +82,11 @@ public class EmployeeController {
 
     @GET
     @Path("employees-projects")
+    @ApiOperation(value = "Get a list of employees along with their working projects")
+    @ApiResponses({
+            @ApiResponse(message = "Return a list of employees with projects", code = 200),
+            @ApiResponse(message = "Something wrong in the server", code = 500)
+    })
     public Response getEmployeesWithProjects(
             @QueryParam("area") @DefaultValue("") String area
     ) {
